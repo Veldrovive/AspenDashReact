b6df064bdda7f87eba2d1f4873939044fc30040d
HeroCC <herocc@herocc.com>
2017-10-02 20:53:59 -0400

    Add an (untested) build & deploy script

---
+++ .circleci/gh-pages-deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -ex
+
+echo "Running from $PWD"
+
+# Setup git so we can use it
+git config --global user.name "CircleCI Deploy script"
+
+# Reset Everything
+git checkout -f
+git checkout master
+
+git fetch origin master
+git reset --hard origin/master
+
+# Move .gitignore out of the way so we can add build/
+mv .gitignore .gitignore.bak
+git add build/
+
+# Commit and push
+git commit -m "Automated React Static build from CircleCI"
+git subtree push --prefix build origin gh-pages
